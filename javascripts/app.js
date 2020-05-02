8781480fb3d856cbe3145c77a0b2a4c10e1dd516
4tnuts <ramanavami32@gmail.com>
2020-05-02 22:26:42 +0800

    change theme button has worked

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,2 +1,11 @@
 const changeThemeBtn = document.getElementsByClassName('change-theme');
-console.log(changeThemeBtn);
+console.log(changeThemeBtn)
+const bodyElement = document.body;
+changeTheme = () => {
+    if(changeThemeBtn[0].textContent == "Change To Black"){
+        changeThemeBtn[0].textContent = "Change To White";
+        return bodyElement.classList.replace("light", "dark");
+    }
+    changeThemeBtn[0].textContent = "Change To Black";
+    bodyElement.classList.replace("dark", 'light');
+};

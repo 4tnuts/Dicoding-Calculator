511c1512afb2b4242898f2a268a841e876a43895
4tnuts <ramanavami32@gmail.com>
2020-05-07 23:20:51 +0800

    still trying to make the logic

---
+++ assets/javascripts/app.js
@@ -0,0 +1,38 @@
+const changeThemeBtn = document.getElementsByClassName("change-theme");
+const screenCalc = document.querySelectorAll(".calculator-screen > p");
+const numberButtons = document.querySelectorAll(".number");
+const aritmethicButtons = document.querySelectorAll(".aritmethic");
+const bodyElement = document.body;
+let aritClicked = 0;
+
+aritmethicButtons.forEach((el, key) => {
+  el.onclick = () => {
+    if (el.textContent === "AC") {
+      return (screenCalc[0].textContent = "");
+    }
+    
+    if (aritClicked == 0 && screenCalc[0].textContent != "") {
+        if(el.textContent === '+' || '-' || '/' || 'x'){
+            aritClicked = 1;
+            return screenCalc[0].textContent += el.textContent;
+        }
+    }
+    return console.log('cant clikced twice');
+  };
+});
+
+numberButtons.forEach((el, key) => {
+  el.onclick = () => {
+    aritClicked = 0;
+    screenCalc[0].textContent += el.textContent;
+  };
+});
+
+const changeTheme = () => {
+  if (changeThemeBtn[0].textContent == "Change To Black") {
+    changeThemeBtn[0].textContent = "Change To White";
+    return bodyElement.classList.replace("light", "dark");
+  }
+  changeThemeBtn[0].textContent = "Change To Black";
+  bodyElement.classList.replace("dark", "light");
+};
